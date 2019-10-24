fix gradient

HW4/cattern/neural_net.py
@@ -105,38 +105,38 @@ class TwoLayerNet(object):
     for index, label in enumerate(y):
         loss -= np.log(softmax_scores[index, label])
     loss /= y.shape[0]
-    loss += reg * 0.5 * (np.sum(W1**2, axis=(0,1)) + np.sum(W2**2, axis=(0,1))) 
-        
+    loss += reg * 0.5 * (np.sum(W1**2, axis=(0,1)) + np.sum(W2**2, axis=(0,1)))
+#     print('SOFTMAX : ',softmax_scores)
     #############################################################################
     #                              END OF TODO#2                                #
     #############################################################################
 
     # Backward pass: compute gradients
     grads = {}
-    #############################################################################
+    ############################################################################
     # TODO#3: Compute the backward pass, computing derivatives of the weights   #
     # and biases. Store the results in the grads dictionary. For example,       #
     # grads['W1'] should store the gradient on W1, and be a matrix of same size #
     # don't forget about the regularization term                                #
     #############################################################################
-    softmax_scores[:N, y] -= 1
+    softmax_scores[np.arange(N), y] -= 1
     softmax_scores /= N
     
     # gradient of W2 
     dW2 = relu_y1.T.dot(softmax_scores)
     # gradient of b2
-    db2 = softmax_scores.sum(axis=1)
+    db2 = softmax_scores.sum(axis=0)
     # gradient of W1
     dW1 = softmax_scores.dot(W2.T)
-    drelu = dW1 * (relu_y1 > 0)
+    drelu = dW1 * (y1 > 0)
     dW1 = X.T.dot(drelu)
     
     # gradient of b1
-    db1 = np.sum(drelu, axis=0)
+    db1 = drelu.sum(axis=0)
     
     # L2 Regularization
-    dW1 += 2 * reg * W1
-    dW2 += 2 * reg * W2
+    dW1 += reg * W1
+    dW2 += reg * W2
     
     grads['W1'] = dW1
     grads['b1'] = db1
@@ -190,6 +190,12 @@ class TwoLayerNet(object):
       # them in X_batch and y_batch respectively.                             #
       # You might find np.random.choice() helpful.                            #
       #########################################################################
+    # random choices of minibatch
+    minibatch_index = np.random.choice(num_train, batch_size)
+    
+    # store X, y according to the choices
+    batchX = X[minibatch_index]
+    batchy = y[minibatch_index]
 
       #########################################################################
       #                             END OF YOUR TODO#4                        #
@@ -205,7 +211,10 @@ class TwoLayerNet(object):
       # using stochastic gradient descent. You'll need to use the gradients   #
       # stored in the grads dictionary defined above.                         #
       #########################################################################
-
+    
+    # loop over to update weights
+    for k in self.params:
+        self.params[k] -= learning_rate * grads[k]
       #########################################################################
       #                             END OF YOUR TODO#5                        #
       #########################################################################
@@ -225,7 +234,8 @@ class TwoLayerNet(object):
         #######################################################################
         # TODO#6: Decay learning rate (exponentially) after each epoch        #
         #######################################################################
-
+        # decay every time at the end of each epoch
+        learning_rate *= learning_rate_decay
         #######################################################################
         #                             END OF YOUR TODO#6                      #
         #######################################################################
@@ -257,7 +267,8 @@ class TwoLayerNet(object):
     ###########################################################################
     # TODO#7: Implement this function; it should be VERY simple!              #
     ###########################################################################
-
+    # choose the maximum probility to be the answer
+    y_pred = np.argmax(self.loss(X), axis=1)
     ###########################################################################
     #                              END OF YOUR TODO#7                         #
     ###########################################################################